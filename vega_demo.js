4f85edb5eca58fb210ff7a6b7297e78cf47577b6
Simon Bonderup Dohn <201707796@post.au.dk>
2022-10-26 09:30:42 +0200

    Removed redundant stuff

--- vega_demo.js
+++
@@ -1,9 +0,0 @@
-// import { v1 } from "@vega/vega-lite-api-v5"
-// import { printTable } from "@uwdata/data-utilities"
-
-function onLoad() {
-    console.log("test");
-    // data = require("vega-datasets@1");
-    // cars = data["cars.json"]();
-    // printTable(cars.slice(0,5));
-}
